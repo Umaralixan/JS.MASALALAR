104353284f68e9552c9b35d48367d1833a90a80d
Umaralixan <umaralixtreme@gmail.com>
2025-04-14 13:25:59 +0500

    index

---
+++ style.js
@@ -0,0 +1,112 @@
+{
+  //   Begin 1
+  //   var a = prompt("a ni kiriritng");
+  //   var p = Number(a) * 4;
+  //   console.log(p);
+}
+
+{
+  //   Begin 2
+  // var a = prompt("a ni kiriritng");
+  // var s = Number(a) * a;
+  // console.log(s);
+}
+
+{
+  //    Begin 3
+//   var a = prompt("a ni kiriritng");
+//   var b = prompt("b ni kiriritng");
+//   var s = Number(a * b);
+//   var p = 2 * (Number(a) + Number(b));
+//   console.log(s);
+//   console.log(p);
+}
+
+{
+  // Begin 4
+  // var d = prompt("d ni kiriting");
+  // var l = 3.14 * d
+  // console.log(l);
+}
+
+{
+  // Begin 5
+  // var a = prompt("a ni kiriting");
+  // var v = Number(a) * a * a;
+  // var s = Number(6) * a * a;
+  // console.log(v);
+  // console.log(s);
+}
+
+{
+  // Begin 6
+  //   var a = prompt("a ni kiriting");
+  //   var b = prompt("b ni kiriting");
+  //   var c = prompt("c ni kiriting");
+  //   var v = a * b * c;
+  //   var s = 2 * (a * b + b * c + a * c);
+  //   console.log(v);
+  //   console.log(s);
+}
+
+{
+  // Begin 7
+  //   var r = prompt("r ni kiriting");
+  //   var l = 2 * 3.14 * r
+  //   var s = 3.14 * r * r;
+  //   console.log(l);
+  //   console.log(s);
+}
+
+{
+  // Begin 8
+  //   var a = prompt("a ni kiriting");
+  //   var b = prompt("b ni kiriting");
+  //   var c = Number(a) * b /2
+  //   console.log(c);
+}
+
+{
+  // Begin 9
+  //   var a = prompt("a ni kiritng");
+  //   var b = prompt("b ni kiritng");
+  //   var geo = Math.sqrt(a * b);
+  //   console.log(geo);
+}
+
+{
+  //   // Begin 10
+  //   var a = prompt("a ni kiriting");
+  //   var b = prompt("b ni kiriting");
+  //   var y = Number(a) + Number(b);
+  //   var k = a * b;
+  //   var kv = a * a;
+  //   var kw = b * b;
+  //   console.log(y);
+  //   console.log(k);
+  //   console.log(kv);
+  //   console.log(kw);
+}
+{
+  // Begin 11
+  //   var a = prompt("a ni kiriting")
+  //   var b = prompt("b ni kiriting")
+  //   var y = Number(a) + Number(b)
+  //   var k = a * b
+  //   var ma = Math.abs(Number(a));
+  //   var mb = Math.abs(Number(b));
+  //   console.log(y);
+  //   console.log(k);
+  //   console.log(ma);
+  //   console.log(mb);
+}
+
+{
+//     Begin 12
+//   var a = prompt("a ni kiriting");
+//   var b = prompt("b ni kiriting");
+//   var c = Math.sqrt(a * b + b * b)
+//   var p = 2 * (Number(a) + Number(b));
+//   console.log(c);
+//   console.log(p);
+}
